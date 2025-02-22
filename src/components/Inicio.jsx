876a63132f9f03716783dc9597eb473ccbde64bf
Luzmilda <az09cylu@gmail.com>
2025-02-22 18:05:55 +0100

    Componente Inicio

---
+++ src/components/Inicio.jsx
@@ -0,0 +1,17 @@
+import '../style/Inicio.css'
+
+function Inicio(){
+    return (
+        <section className="home" id="home">
+
+            <div className="content">
+                <span>Bienvenidos a Euphoria SexToys</span>
+                <h3>JUGUETES Y LENCERIA</h3>
+                <p>Explora un mundo de placer en nuestra tienda de juguetes y lencería para adultos. Ofrecemos una cuidada selección de vibradores, consoladores y lencería exquisita, todos elegidos por su calidad y seguridad. Ya sea que busques nuevas sensaciones o desees sorprender a tu pareja, aquí encontrarás todo lo necesario para encender la pasión y disfrutar de momentos inolvidables.</p>
+                <a href="#products" className="btn">Comprar Ahora</a>
+            </div>
+        </section>
+    )
+}
+
+export default Inicio;
